3b096dfa12b2208ea22c565ac33493548308099e
teamdreamrus <atazhanov.biz@gmail.com>
2020-07-21 16:20:42 +0700

    close onClick

--- static/content/content.js
+++ static/content/content.js
@@ -9,7 +9,11 @@ chrome.runtime.onMessage.addListener(function(
   let close = document.createElement('img');
   close.src = 'https://image.flaticon.com/icons/svg/748/748122.svg';
   close.className = 'img-close';
+  close.onclick = function() {
+    node.style.display = 'none';
+    // save about Close
+  };
+
   node.appendChild(close);
   document.body.appendChild(node);
-  console.log(request.message);
 });
